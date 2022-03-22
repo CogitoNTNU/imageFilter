335fea0b3b7692a2a338c5f16e7c8d964633261f
ambe98 <amundb2010@hotmail.com>
2022-03-22 19:47:36 +0100

    Update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -2,6 +2,10 @@ import os
 import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
 
 class CustomImageDataset(Dataset):
     def __init__(self, img_dir, transform=None, target_transform=None):
@@ -18,8 +22,27 @@ class CustomImageDataset(Dataset):
         image = read_image(img_path)
         if self.transform:
             image = self.transform(image)
+        if image.size()[0] == 1: image= image[0].repeat(3, 1, 1)
         return image
     
-    
-# dataset = CustomImageDataset('.\\style_images\\test_samples')
+
+
+if __name__ == '__main__':
+    dataset = CustomImageDataset('.\\style_images\\test_samples',transform = transforms.Resize((512, 640)))
+
+    bs = 10
+    dataloader = DataLoader(dataset, batch_size=bs, shuffle=True)
+    iterator = iter(dataloader)
+    for i in range(10):
+        images = next(iterator)
+        print(f"Feature batch shape: {images.size()}")
+
+        fig, axs = plt.subplots(bs,1,figsize=(5, 5*bs))
+        for i in range(bs):
+            img = images[i].permute(1,2,0).squeeze()
+            axs[i].axis("off")
+            axs[i].imshow(img)
+        plt.show()
+
+
 
