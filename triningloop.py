02410df650b8a1152b34f1b548e504fc41a92144
erlendhep <89533983+erlendhep@users.noreply.github.com>
2022-04-05 19:05:59 +0200

    Savefig hver x epoke

--- triningloop.py
+++ triningloop.py
@@ -4,10 +4,6 @@ from custom_dataset import*
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-
-
-
     encoder = Encoder(blocks=12)  # IDEAL BLOCKS ARE (depending on inclusion of 1 conv before output)5/6, 11/12, 19/20,
     adain = AdaIN()
     decoder = Decoder(blocks=12)
@@ -35,11 +31,12 @@ if __name__ == '__main__':
     dataset_content = CustomImageDataset('style_images/test_samples',transform)
 
 
-    batch_size = 5
+    batch_size = 10
     dataloader_style = DataLoader(dataset_style, batch_size=batch_size, shuffle=True)
     dataloader_content = DataLoader(dataset_content, batch_size=batch_size, shuffle=True)
 
-    for epoch in range(5):
+    n_epochs = 2
+    for epoch in range(n_epochs):
 
         #style_img = TF.resize(TF.to_tensor(style_img), (512, 640))  # TODO: Automatically find the needed size based on VGG-blocks used
         #content_img = TF.resize(TF.to_tensor(content_img), (512, 640))  # TODO: Automatically find the needed size based on VGG-blocks used
@@ -64,6 +61,12 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if epoch % 99 == 0:
-                plt.imshow(decoder_out.detach().cpu().squeeze(dim=0).numpy().transpose(1, 2, 0))
-                plt.show()
+
+        if epoch %5 == 0: ##Endre på hvor ofte du ønsker å lagre bildet
+            img_tensor = decoder_out.detach().cpu().squeeze(dim=0)
+            fig, axs = plt.subplots(batch_size,1,figsize=(5, 5*batch_size))
+            for i in range(batch_size):
+                img = img_tensor[i].permute(1,2,0).squeeze()
+                axs[i].axis("off")
+                axs[i].imshow(img)
+            plt.savefig(f"progressimages/epoch{epoch}.png")
