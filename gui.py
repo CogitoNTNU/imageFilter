1c073e52c7f1b78dcb3b76de4baf76aebeb8bf73
ambe98 <amundb2010@hotmail.com>
2022-04-19 20:39:06 +0200

    small fix by you!

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 from trined_model import TrinedModel
 
 class StyleTransferGUI():
-    def __init__(self, model, width=700, height=400):
+    def __init__(self, model, width=1100, height=400):
         self.image1 = None
         self.image2 = None
         self.output_image = None
@@ -20,9 +20,10 @@ class StyleTransferGUI():
         self.canvas = tk.Canvas(self.root, width=width, height=height)
         self.canvas.pack()
         self.image_size = (300,300)
-        self.upload_b1 = tk.Button(self.root, text="Upload image 1", width=30, command = lambda:self.upload_im_1())
+        self.image_size_output = (330,330)
+        self.upload_b1 = tk.Button(self.root, text="Upload style image", width=30, command = lambda:self.upload_im_1())
         self.upload_b1.pack()
-        self.upload_b2  = tk.Button(self.root, text="Upload image 2", width=30, command = lambda:self.upload_im_2())
+        self.upload_b2  = tk.Button(self.root, text="Upload content image", width=30, command = lambda:self.upload_im_2())
         self.upload_b2.pack()
         self.switch_input_b  = tk.Button(self.root, text="Switch input", width=30, command = lambda:self.switch_image())
         self.switch_input_b.pack()
@@ -32,6 +33,12 @@ class StyleTransferGUI():
         self.run_text.set(self.default_message)
         self.run_b = tk.Button(self.root, textvariable=self.run_text, width=30, command = lambda:self.combine_images())
         self.run_b.pack()
+        self.style_txt = tk.Label(self.root, text="Style",font=("Helvetica", 18))
+        self.style_txt.place(x=120,y=self.height*(6/7))
+        self.content_txt = tk.Label(self.root, text="Content",font=("Helvetica", 18))
+        self.content_txt.place(x=self.width-120-20,y=self.height*(6/7))
+        self.output_txt = tk.Label(self.root, text="This is art!",font=("Helvetica", 18))
+
         self.model = model
         
     def switch_image(self):
@@ -49,7 +56,7 @@ class StyleTransferGUI():
             self.canvas.image2 = self.tk_im2
         if self.output_image is not None:
             self.tk_imO = ImageTk.PhotoImage(self.output_image)
-            self.canvas.create_image(self.width/2, self.height-50,anchor=tk.CENTER, image=self.tk_imO)    
+            self.canvas.create_image(self.width/2,170 ,anchor=tk.CENTER, image=self.tk_imO)    
             self.canvas.imageO = self.tk_imO
         
     def upload_im_1(self):
@@ -72,7 +79,9 @@ class StyleTransferGUI():
             self.run_text.set(self.error_message)
         else:
             self.run_text.set(self.default_message)
-            self.output_image = model.forward(self.image1, self.image2)
+            self.output_image = model.forward(self.image1, self.image2).resize(self.image_size_output)
+
+            self.output_txt.place(x=self.width/2-20,y=self.height*(6/7))
             self.update()
     def run(self):
         self.root.mainloop()
--- trined_model.py
+++ trined_model.py
@@ -3,14 +3,19 @@ from PIL import Image
 from os.path import exists
 import torch
 from torchvision import transforms
+from modules import*
 
 
 class TrinedModel(ModelInterface):
     def __init__(self):
-        self.model_path = "Martin.model"
+        self.model_path = "trained_model/adain_model_9"
         super().__init__()
 
-        self.transform = transforms.Compose([transforms.Resize((512, 640)), transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        self.transform = transforms.Compose([
+            transforms.Resize((512, 640)), 
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
 
         self.encoder = Encoder(blocks=12)  # IDEAL BLOCKS ARE (depending on inclusion of 1 conv before output)5/6, 11/12, 19/20,
         self.adain = AdaIN()
@@ -30,16 +35,16 @@ class TrinedModel(ModelInterface):
         except:
             raise Exception(f"Error in transform of images.")
 
-        encoder_out_style = self.encoder(style_img)      # TODO: Pass both images through encoder in one forward pass
-        encoder_out_content = self.encoder(content_img)
+        encoder_out_style = self.encoder(style_img.unsqueeze(0))      # TODO: Pass both images through encoder in one forward pass
+        encoder_out_content = self.encoder(content_img.unsqueeze(0))
         adain_out = self.adain(encoder_out_content, encoder_out_style)
         decoder_out = self.decoder(adain_out)
 
         MEAN = torch.tensor([0.485, 0.456, 0.406])
         STD = torch.tensor([0.229, 0.224, 0.225])
 
-        img = decoder_out * STD[:, None, None] + MEAN[:, None, None]
-        img = transforms.ToPILImage(img)
+        img = decoder_out[0] * STD[:, None, None] + MEAN[:, None, None]
+        img = transforms.ToPILImage()(img)
 
         return img
 
